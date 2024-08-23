85e060d14b9ee1794c0e8ee43c57148e2ad68120
SecretPasta <boris.shoostin@gmail.com>
2024-08-23 11:24:35 +0300

    preventing fire when reloading

--- Source/Blaster/BlasterComponents/CombatComponent.cpp
+++ Source/Blaster/BlasterComponents/CombatComponent.cpp
@@ -116,7 +116,7 @@ void UCombatComponent::MulticastFire_Implementation(const FVector_NetQuantize& T
 	if (EquippedWeapon == nullptr) {
 		return;
 	}
-	if (Character) {
+	if (Character && CombatState == ECombatState::ECS_Unoccupied) {
 		Character->PlayFireMontage(bAiming);
 		EquippedWeapon->Fire(TraceHitTarget);
 	}
@@ -184,6 +184,9 @@ void UCombatComponent::FinishReloading()
 	if (Character->HasAuthority()) {
 		CombatState = ECombatState::ECS_Unoccupied;
 	}
+	if (bFireButtonPressed) {
+		Fire();
+	}
 }
 
 void UCombatComponent::OnRep_CombatState()
@@ -191,6 +194,11 @@ void UCombatComponent::OnRep_CombatState()
 	switch (CombatState) {
 	case ECombatState::ECS_Reloading:
 		HandleReload();
+		break;
+	case ECombatState::ECS_Unoccupied:
+		if (bFireButtonPressed) {
+			Fire();
+		}
 		break;
 	}
 }
@@ -365,7 +373,7 @@ bool UCombatComponent::CanFire()
 		return false;
 	}
 
-	return !EquippedWeapon->IsEmpty() || !bCanFire;
+	return !EquippedWeapon->IsEmpty() && bCanFire && CombatState == ECombatState::ECS_Unoccupied;
 }
 
 void UCombatComponent::OnRep_CarriedAmmo()
