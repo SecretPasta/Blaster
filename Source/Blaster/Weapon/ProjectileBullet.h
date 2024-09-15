0bc716c346050578764c9da81f9656fdbcb90ab5
SecretPasta <boris.shoostin@gmail.com>
2024-09-15 21:05:42 +0300

    Added post edit change property

--- Source/Blaster/Weapon/ProjectileBullet.h
+++ Source/Blaster/Weapon/ProjectileBullet.h
@@ -17,6 +17,10 @@ public:
 
 	AProjectileBullet();
 
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& Event) override;
+#endif
+
 protected:
 
 	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) override;
